e80482cc299cd7a37a4bda76759bc46bc6b79e78
SUNDAYTITILAYO <tititopesunday@gmail.com>
2024-12-16 16:17:48 +0000

    Update strmlit_cld_code.py

--- strmlit_cld_code.py
+++ strmlit_cld_code.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
